02a2e94f7a1b6c55bb63295dd9106cbc659532cd
Mohd Khairi Mohd Yusoff <102360814+KhairiYusoff@users.noreply.github.com>
2022-11-18 16:14:49 +0800

    updated

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -12,4 +12,42 @@ const MeetupDetails = () => {
     )
 }
 
+export async function getStaticPaths() {
+    return {
+        fallback: false,
+        paths: [
+            {
+                params: {
+                    meetupId: 'm1',
+                }
+            },
+            {
+                params: {
+                    meetupId: 'm2',
+                }
+            }
+        ]
+    }
+}
+
+export async function getStaticProps(context) {
+    //fetch data for a single meetup
+
+    const meetupId = context.params.meetupId;
+
+    console.log(meetupId)
+
+    return {
+        props: {
+            meetupData: {
+                image: "https://images.unsplash.com/photo-1486571698588-a2204703bec8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1331&q=80",
+                id: meetupId,
+                title: "A First Meetup",
+                address: "Some address 10, 12345, Portugal",
+                description: "Tourists on a sandy shore"
+            }
+        }
+    }
+}
+
 export default MeetupDetails;
