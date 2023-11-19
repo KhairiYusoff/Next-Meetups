7c037d54ffd9de9bc989cebbb05e19763aec3b63
Mohd Khairi Mohd Yusoff <102360814+KhairiYusoff@users.noreply.github.com>
2023-11-19 23:28:10 +0800

    Implement dynamic routing and MongoDB data fetching

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,33 +1,48 @@
+import { MongoClient, ObjectId } from 'mongodb';
 
 import MeetupDetail from '../../components/meetups/MeetupDetail';
 
-const MeetupDetails = () => {
+const MeetupDetails = (props) => {
     return (
         <MeetupDetail
-            image="https://images.unsplash.com/photo-1486571698588-a2204703bec8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1331&q=80"
-            title="A First Meetup"
-            address="Some address 10, 12345, Portugal"
-            description="Tourists on a sandy shore"
+            image={props.meetupData.image}
+            title={props.meetupData.title}
+            address={props.meetupData.address}
+            description={props.meetupData.description}
         />
     )
 }
 
 export async function getStaticPaths() {
+
+    const client = await MongoClient.connect('mongodb+srv://khairi:tabligh@cluster0.turpfle.mongodb.net/meetups?retryWrites=true&w=majority');
+
+    const db = client.db();
+
+    const meetupsCollection = db.collection('meetups');
+
+    const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray()
+
+    client.close()
+
     return {
         fallback: false,
-        paths: [
-            {
-                params: {
-                    meetupId: 'm1',
-                }
-            },
-            {
-                params: {
-                    meetupId: 'm2',
-                }
-            }
-        ]
+        paths: meetups.map(meetup => ({ params: { meetupId: meetup._id.toString() } }))
+
+        // [
+        //     {
+        //         params: {
+        //             meetupId: 'm1',
+        //         }
+        //     },
+        //     {
+        //         params: {
+        //             meetupId: 'm2',
+        //         }
+        //     }
+        // ]
     }
+
 }
 
 export async function getStaticProps(context) {
@@ -35,17 +50,32 @@ export async function getStaticProps(context) {
 
     const meetupId = context.params.meetupId;
 
-    console.log(meetupId)
+    const client = await MongoClient.connect('mongodb+srv://khairi:tabligh@cluster0.turpfle.mongodb.net/meetups?retryWrites=true&w=majority');
+
+    const db = client.db();
+
+    const meetupsCollection = db.collection('meetups');
+
+    const selectedMeetup = await meetupsCollection.findOne({ _id: ObjectId(meetupId) })
+
+    client.close()
 
     return {
         props: {
             meetupData: {
-                image: "https://images.unsplash.com/photo-1486571698588-a2204703bec8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1331&q=80",
-                id: meetupId,
-                title: "A First Meetup",
-                address: "Some address 10, 12345, Portugal",
-                description: "Tourists on a sandy shore"
+                id: selectedMeetup._id.toString(),
+                title: selectedMeetup.title,
+                address: selectedMeetup.address,
+                image: selectedMeetup.image,
+                description: selectedMeetup.description
             }
+            // {
+            //     image: "https://images.unsplash.com/photo-1486571698588-a2204703bec8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1331&q=80",
+            //     id: meetupId,
+            //     title: "A First Meetup",
+            //     address: "Some address 10, 12345, Portugal",
+            //     description: "Tourists on a sandy shore"
+            // }
         }
     }
 }
