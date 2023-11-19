5188d5e8e1afeadf9d397aab44aab9bda3ab99a7
Mohd Khairi Mohd Yusoff <102360814+KhairiYusoff@users.noreply.github.com>
2023-11-19 22:53:30 +0800

    Close MongoDB client connection

--- pages/index.js
+++ pages/index.js
@@ -60,7 +60,7 @@ export async function getStaticProps() {
 
     const meetups = await meetupsCollection.find().toArray();
 
-    console.log(meetups)
+    client.close()
 
     return {
         props: {
